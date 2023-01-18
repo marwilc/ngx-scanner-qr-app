28a1418265dfe9cee20af6eb7b47b4fcdbe5b46d
Marwil Campos <marwilcampos@gmail.com>
2023-01-18 11:40:30 -0400

    :sparkles: added scanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    ViewChild,
+    ViewEncapsulation,
+} from '@angular/core';
+import { QrScannerComponent } from 'angular2-qrscanner';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'scanner-qr-app';
+    title = 'scanner-qr-app';
+    @ViewChild(QrScannerComponent)
+    qrScannerComponent: QrScannerComponent;
+
+    ngAfterViewInit(): void {
+        //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+        //Add 'implements AfterViewInit' to the class.
+
+        console.log(this.qrScannerComponent);
+
+        this.qrScannerComponent.getMediaDevices().then((devices) => {
+            console.log(devices);
+            const videoDevices: MediaDeviceInfo[] = [];
+            for (const device of devices) {
+                if (device.kind.toString() === 'videoinput') {
+                    videoDevices.push(device);
+                }
+            }
+            if (videoDevices.length > 0) {
+                let choosenDev;
+                for (const dev of videoDevices) {
+                    if (dev.label.includes('front')) {
+                        choosenDev = dev;
+                        break;
+                    }
+                }
+                if (choosenDev) {
+                    this.qrScannerComponent.chooseCamera.next(
+                        choosenDev
+                    );
+                } else {
+                    this.qrScannerComponent.chooseCamera.next(
+                        videoDevices[0]
+                    );
+                }
+            }
+        });
+
+        this.qrScannerComponent.capturedQr.subscribe((result) => {
+            console.log(result);
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgQrScannerModule } from 'angular2-qrscanner';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, NgQrScannerModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
